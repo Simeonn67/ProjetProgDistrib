a1a9061c3202d600095d33d8e906ab9ef6a73bca
Simeonn <jer.o@free.fr>
2013-12-12 16:18:44 +0100

    yep

---
+++ client_src/client.c
@@ -0,0 +1,149 @@
+#include "person.h"
+//#include "xdr_entiers2.h"
+#include <rpc/types.h>
+#include <rpc/xdr.h>
+#include <rpc/rpc.h>
+
+#define MAX_POP 60
+
+
+t_door* theDoor;
+t_dot2d theFlag;
+t_obstacle* tabObs[_MAX_OBSTACLE];
+t_person* village[MAX_POP];
+
+
+#define _SCREEN_HEIGHT 720
+#define _SCREEN_WIDTH 1280
+
+void displayVillage()
+{
+	for(int i=0;i<MAX_POP;i++)
+	{
+		printf("%d[",i);
+		for(int j=0;j<_MAX_PATH_SIZE;j++)
+			printf("%d",village[i].path[j]);
+		printf("]\n\n\n");
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	srand(time(NULL));
+	int i,j,randomInteger;
+  for(i=0;i<MAX_POP;i++)
+    {
+/*		  direction randomDirection;
+  int randomInteger;
+
+  current = 0;
+  mark = -1;
+
+//  srand (time(NULL));
+
+  for(int i=0;i<_MAX_PATH_SIZE;i++)
+    {
+      randomInteger = rand() % 8 + 1;
+      randomDirection = static_cast<direction>(randomInteger);
+      path[i] = randomDirection;
+    }
+*/
+		village[i].current=0;
+		village[i].mark=-1;
+		for(j=0;j<_MATH_PATH_SIZE;i++)
+		{
+			randomInteger=rand()%8+1;
+			village[i].path[j]=randomInteger;
+		}
+      //village[i] = new Person();
+    }
+  int newObsSize = 0;
+  int newObsPosX = 0;
+  int newObsPosY = 0; 
+  t_obstacle* newObstacle;
+  bool newObsInCollision = false;
+  int nbObs=0; //number of obstacle
+  bool posAvailable;
+	//SDL_Surface* screen;
+	//theDoor = new Door(screen, _SCREEN_WIDTH, _SCREEN_HEIGHT);
+
+  theFlag.x=100;
+  theFlag.y=(_SCREEN_HEIGHT/2) - 138/2;
+
+  srand (time(NULL));
+
+	/*for(i=0;i<_MAX_OBSTACLE;i++)
+    {
+      newObsSize = rand() % 10 + 1;
+      newObstacle = new Obstacle(screen, newObsSize);
+
+      posAvailable = false;
+
+      cout << " \n Obs size is : " << newObsSize*20 << "\n" ;
+
+      while(posAvailable == false)
+	{
+	  posAvailable = true;
+	  newObsPosX = rand() % ((3*_SCREEN_WIDTH)/4) + (_SCREEN_WIDTH/4) ;
+	  newObsPosY = rand() % _SCREEN_HEIGHT;
+	  newObstacle->setPosition(newObsPosX,newObsPosY);
+
+	  for(int y=0;y<nbObs;y++)
+	    {
+	      if(newObstacle->isInCollision(tabObs[y]) || theDoor->isInCollision(newObstacle) )
+		{
+		  posAvailable = false;
+		  break;
+		}
+	    }
+	}
+      
+      nbObs++;
+      tabObs[i] = new Obstacle(screen, newObsSize);
+      tabObs[i]->setPosition(newObsPosX,newObsPosY);
+      delete newObstacle;
+    }*/
+    //printf("%d",village[1]->getMark());
+	//village[1]->calculMark(tabObs,*theDoor,theFlag);
+	
+//	displayVillage();
+	for(int i=0;i<MAX_POP;i++)
+	{
+		village[i].mark=calculMark(village[i],tabObs,*theDoor,theFlag);
+		
+		printf("%d\n",village[i].mark);
+	}
+	//printf("%d",village[1]->getMark());
+		//displayVillage();
+
+/*  char *host = argv[1];
+  enum clnt_stat stat;*/
+	/*deuxmatricesopeplus aEnvoyer = {mat1,mat2,1,taille};
+	if(atoi(argv[2])==1)
+	{
+		printf("1\n");
+		aEnvoyer.ope=1;
+	}
+	else
+	{
+		printf("2\n");
+		aEnvoyer.ope=2;
+	}*/
+  /*stat = callrpc(host,PROGNUM,VERSNUM,PROCNUM,(xdrproc_t)xdr_entiers2,(char*)&donnees,(xdrproc_t)xdr_entiers2,(char*)&donnees); 
+  if (stat != RPC_SUCCESS) { 
+    fprintf(stderr, "Echec de l'appel distant\n");
+    clnt_perrno(stat);      fprintf(stderr, "\n");
+  } else {
+	  printf("Return!\n");
+  }
+  return(0);*/
+
+
+
+  for(int j=0;j<MAX_POP;j++)
+    {
+      free(village[j]);
+    }
+
+};
+
