39b6a9126b87f8fc916ff4588349cdb42548a41b
PsYcOz <cataldo.buisnessContact@gmail.com>
2013-12-12 15:48:51 +0100

    Jumbo

--- client_inc/data.h
+++ client_inc/data.h
@@ -5,7 +5,7 @@
 ** Contact   <cataldo.nico@gmail.com>
 ** 
 ** Started on  Thu Dec 12 08:06:49 2013 Nicolas Cataldo
-** Last update Thu Dec 12 08:38:33 2013 Nicolas Cataldo
+** Last update Thu Dec 12 15:41:27 2013 Nicolas Cataldo
 */
 
 #ifndef			_DATA_H_
@@ -16,6 +16,12 @@
 #include		<string.h>
 #include		<math.h>
  
+enum e_obs_size{size01 = 020, size02 = 040, 
+		size03 = 060, size04 = 080,
+		size05 = 100, size06 = 120, 
+		size07 = 140, size08 = 160,
+		size09 = 180, size10 = 200};
+
 typedef struct          s_dot2d
 {
   int 			x;
@@ -29,16 +35,17 @@ typedef struct		s_door
 {
   unsigned short int   	doorHeight;
   unsigned short int	doorWidth;
-  t_dot2d		*doorCenter;
+  t_dot2d		doorSource;
+  t_dot2d		doorCenter;
 }			t_door;
 
-enum e_obs_size{size01 = 20, size02 = 40, size03 = 60, size04 = 80, size05 = 100,
-	     size06 = 120, size07 = 140, size08 = 160, size09 = 180, size10 = 200};
+
 
 typedef struct		s_obstacle
 {
   e_obs_size		obsSize;
   int			obsRadius;
+  t_dot2d		obsSource;
   t_dot2d		obsCenter;
 }			t_obstacle;
 
